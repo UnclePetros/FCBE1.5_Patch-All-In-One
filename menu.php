c024fe4dbe6dd1f5bdf3628081d331f0430808fb
UnclePetros <UnclePetros@users.noreply.github.com>
2015-08-19 10:25:12 +0000

    Aggiornato alla rev. assembla 37

--- menu.php
+++ menu.php
@@ -277,21 +277,6 @@ if ($chiusura_giornata == 1) echo "<a href='./registro_mercato.php' >Registro me
 	<caption>SPONSORS</caption><tr><td align='center'>
 	<br/>
 	<script type='text/javascript'>
-	<!--
-	google_ad_client = 'pub-9997862776187032';
-	google_ad_width = 180;
-	google_ad_height = 150;
-	google_ad_format = '180x150_as';
-	google_ad_type = 'text_image';
-	google_ad_channel = '';
-	google_color_border = 'FFFFFF';
-	google_color_bg = 'FFFFFF';
-	google_color_link = 'CC0000';
-	google_color_text = '6F3C1B';
-	google_color_url = '940F04';
-	//-->
-	</script>
-	<script type='text/javascript'src='http://pagead2.googlesyndication.com/pagead/show_ads.js'>
 	</script>
 	<br/><br/>
 	</td></tr></table>";
